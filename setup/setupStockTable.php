13a01728bbe8ddad168af2c0970136fe2856bdf3
lmuir2021 <81841185+lmuir2021@users.noreply.github.com>
2021-06-09 10:19:04 +1000

    Created files to setup database
    Since the computer I was working on was wiped, I was hit by the realisation of the need to have a way to automatically setup the entire SQL database without going through PHPMyAdmin every time, which was slow.

---
+++ setup/setupStockTable.php
@@ -0,0 +1,41 @@
+<?php
+
+$servername = "localhost"; // The server name containing the database
+$username = "lmuir2021";
+$password = "riddles";
+$databaseName = "QSIMDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $databaseName);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO `stock` (`item`) VALUES 
+    ('DPCU Pants'),
+    ('DPCU Shirt'),
+    ('Black Belt'),
+    ('Belt Brass'),
+    ('DPCU Field Hat'),
+    ('Khaki Fur Felt'),
+    ('Cadet Jumper'),
+    ('Cadet Boots'),
+    ('ANF Patch'),
+    ('AAC Patch'),
+    ('2nd BDE Patch'),
+    ('Hutchie'),
+    ('Poncho'),
+    ('Duffle Bag'),
+    ('Kidney Cup'),
+    ('Water Bottle');";
+
+$result = $conn->query($sql);
+if ($result == TRUE) {
+    echo "Good";
+} else {
+    echo "Bad";
+}
+
+?>
