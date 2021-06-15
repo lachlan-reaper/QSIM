4147ed67778896625f41fc0a23ea7686fdbd7290
lmuir2021 <81841185+lmuir2021@users.noreply.github.com>
2021-06-15 18:17:56 +1000

    Adding session start

--- setup/setupAll.php
+++ setup/setupAll.php
@@ -1,12 +1,11 @@
 <?php
     $servername = "localhost"; // The server name containing the database
-    $username = "admin";
-    $password = "secret";
+    $username = "root";
 
     $databaseName = "QSIMDB";
     
     // Create connection
-    $conn = new mysqli($servername, $username, $password);
+    $conn = new mysqli($servername, $username);
     
     // Check connection
     if ($conn->connect_error) {
@@ -14,14 +13,28 @@
     }
     // NEED TO ADD THE lmuir2021 USER WITH riddles PASSWORD!!!!
 
-    $sql = "CREATE DATABASE '$databaseName';";
+    $sql = "CREATE DATABASE `$databaseName`;";
     
     $result = $conn->query($sql);
     if ($result == TRUE) {
         echo "Good";
     } else {
         echo "Bad";
+        die();
     }
+    echo "<br>";
+
+    $sql = "CREATE USER lmuir2021 IDENTIFIED BY 'riddles';
+    GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, INDEX ON `$databaseName`.* TO lmuir2021;";
+    
+    $result = $conn->multi_query($sql);
+    if ($result == TRUE) {
+        echo "Good";
+    } else {
+        echo "Bad";
+        die($sql);
+    }
+    echo "<br>";
 
     require 'setupDatabase.php';
     require 'setupStockTable.php';
